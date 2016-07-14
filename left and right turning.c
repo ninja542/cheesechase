4764fbc62d483de84fc7d802c7cdf42b25ed7c2e
ninja542 <theroboticcyborg@gmail.com>
2016-07-14 14:03:56 -0400

    Add files via upload

--- left and right turning.c
+++ left and right turning.c
@@ -14,13 +14,16 @@ nVolume = 4; //resets sound volume so woops can play.
 
 while(true)
 {
-	if(SensorValue(frontSonicSensor) < 13) //if front ultrasonic sensor senses a wall, decide whether to turn left or right
+	if(SensorValue(frontSonicSensor) < 14) //if front ultrasonic sensor senses a wall, decide whether to turn left or right
 	{
 		if(SensorValue(rightSonicSensor) < 11) // if right is less than 10 cm, then turn left
 		{
-			numCorners = numCorners + 1;
+			motor[motorC] = 0;
+			motor[motorB] = 0;
+			wait1Msec(100); //make sure it's at full stop so it can turn accurately
+			numCorners = numCorners + 1; //add 1 to numCorners variable for counting corners
 			nMotorEncoder[motorC] = 0;
-			while(nMotorEncoder[motorC] < 180)
+			while(nMotorEncoder[motorC] < 170)
 			{
 				motor[motorC] = 50;
 				motor[motorB] = -50;
@@ -31,7 +34,7 @@ while(true)
 		wait1Msec(100); //to make sure it doesn't accidentally sense the walls again and get confused
 		if(SensorValue(rightSonicSensor) > 11) //if right ultrasonic sensor detects more than 15 cm, turn right
 		{
-			numCorners = numCorners + 1;
+			numCorners = numCorners + 1; //ad 1 to numCorners variable for counting corners
 			nMotorEncoder[motorB] = 0;
 			while(nMotorEncoder[motorB] < 150)
 			{
@@ -47,30 +50,26 @@ while(true)
 			motor[motorC] = -100;
 			motor[motorB] = 0;
 		}
-		wait1Msec(500);
 
 		if(SensorValue(rightLightSensor) > 41)
 		{
 			motor[motorC] = 0;
 			motor[motorB] = -100;
 		}
-		wait1Msec(500);
 
 		if(SensorValue(leftLightSensor) < 41) // controls left light sensor for line following like above, but in the opposite direction
 		{
 			motor[motorC] = 0;
 			motor[motorB] = -100;
 		}
-		wait1Msec(500);
 
 		if(SensorValue(leftLightSensor) > 41)
 		{
 			motor[motorC] = -100;
 			motor[motorB] = 0;
 		}
-		wait1Msec(500);
 
-		if(SensorValue(rightLightSensor) > 60 && SensorValue(leftLightSensor) > 60)
+		if(SensorValue(rightLightSensor) > 60 && SensorValue(leftLightSensor) > 60) //detects the aluminum foil
 		{
 			motor[motorA] = 100;
 			PlaySoundFile("Woops.rso"); //make a sound
@@ -81,15 +80,19 @@ while(true)
 		{
 			if(SensorValue(rightLightSensor) < 41 && SensorValue(leftLightSensor) < 41) //sees if it has crossed the green line yet
 			{
+				motor[motorB] = 0;
+				motor[motorC] = 0;
+				wait1Msec(100); //braking so it goes straighter and doesn't run off track
+
 				nMotorEncoder[motorC] = 0;
-				while(nMotorEncoder[motorC] > -360)
+				while(nMotorEncoder[motorC] > -360) //moves forward one rotation
 				{
 					motor[motorC] = -100;
 					motor[motorB] = -100;
 				}
 			motor[motorC] = 0;
 			motor[motorB] = 0;
-			nxtDisplayStringAt(0, 31, "Corners = %d", numCorners); //display # of corners
+			nxtDisplayStringAt(0, 31, "Corners = %d", numCorners); //displays # of corners
 			wait1Msec(10000);
 			}
 		}
