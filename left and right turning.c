cb38b867c1826578853d8fc078951d73b862900b
ninja542 <theroboticcyborg@gmail.com>
2016-07-15 10:18:54 -0400

    Update left and right turning.c

--- left and right turning.c
+++ left and right turning.c
@@ -8,7 +8,7 @@ task main()
 {
 
 int numCorners;
-numCorners = 0;
+numCorners = 0; // variable for counting corners
 
 nVolume = 4; //resets sound volume so woops can play.
 
@@ -16,27 +16,31 @@ while(true)
 {
 	if(SensorValue(frontSonicSensor) < 14) //if front ultrasonic sensor senses a wall, decide whether to turn left or right
 	{
-		if(SensorValue(rightSonicSensor) < 11) // if right is less than 10 cm, then turn left
+		if(SensorValue(rightSonicSensor) < 11) // if right is less than 11 cm, then turn left
 		{
 			motor[motorC] = 0;
 			motor[motorB] = 0;
 			wait1Msec(100); //make sure it's at full stop so it can turn accurately
 			numCorners = numCorners + 1; //add 1 to numCorners variable for counting corners
-			nMotorEncoder[motorC] = 0;
-			while(nMotorEncoder[motorC] < 170)
+			motor[motorC] = 50;
+			motor[motorB] = -50;
+			wait1Msec(200); //pause so light sensor doesn't sense the original line
+			while(SensorValue(leftLightSensor) > 41) //turns until left light sensor sees black line
 			{
-				motor[motorC] = 50;
-				motor[motorB] = -50;
+				motor[motorC] = 45;
+				motor[motorB] = -45;
 			}
 		}
 		motor[motorC] = 0;
 		motor[motorB] = 0;
-		wait1Msec(100); //to make sure it doesn't accidentally sense the walls again and get confused
-		if(SensorValue(rightSonicSensor) > 11) //if right ultrasonic sensor detects more than 15 cm, turn right
+		wait1Msec(250); //to make sure it doesn't accidentally sense the walls again and get confused
+		if(SensorValue(rightSonicSensor) > 11) //if right ultrasonic sensor detects more than 11 cm, turn right
 		{
-			numCorners = numCorners + 1; //ad 1 to numCorners variable for counting corners
-			nMotorEncoder[motorB] = 0;
-			while(nMotorEncoder[motorB] < 150)
+			numCorners = numCorners + 1; //add 1 to numCorners variable for counting corners
+			motor[motorC] = -50;
+			motor[motorB] = 50;
+			wait1Msec(200); //same turning as above.
+			while(SensorValue(rightLightSensor) > 41) //turns until sensor senses black line
 			{
 				motor[motorC] = -50;
 				motor[motorB] = 50;
@@ -47,26 +51,26 @@ while(true)
 	{
 		if(SensorValue(rightLightSensor) < 41) // controls right light sensor for line following
 		{
-			motor[motorC] = -25;
+			motor[motorC] = -50;
 			motor[motorB] = 0;
 		}
 
 		if(SensorValue(rightLightSensor) > 41)
 		{
-			motor[motorC] = -25;
-			motor[motorB] = -25;
+			motor[motorC] = -50;
+			motor[motorB] = -50;
 		}
 
 		if(SensorValue(leftLightSensor) < 41) // controls left light sensor for line following like above, but in the opposite direction
 		{
 			motor[motorC] = 0;
-			motor[motorB] = -25;
+			motor[motorB] = -50;
 		}
 
 		if(SensorValue(leftLightSensor) > 41)
 		{
-			motor[motorC] = -25;
-			motor[motorB] = -25;
+			motor[motorC] = -50;
+			motor[motorB] = -50;
 		}
 
 		if(SensorValue(rightLightSensor) > 60 && SensorValue(leftLightSensor) > 60) //detects the aluminum foil
@@ -91,9 +95,13 @@ while(true)
 					motor[motorB] = -100;
 				}
 			motor[motorC] = 0;
-			motor[motorB] = 0;
+			motor[motorB] = 0; //brake at end zone
 			nxtDisplayStringAt(0, 31, "Corners = %d", numCorners); //displays # of corners
-			wait1Msec(10000);
+			PlaySound(soundBeepBeep);
+			PlaySound(soundFastUpwardTones);
+			PlaySound(soundDownwardTones);
+			PlaySound(soundLowBuzz);
+			wait1Msec(20000); //keeps display + light on for 20 sec
 			}
 		}
 	}
